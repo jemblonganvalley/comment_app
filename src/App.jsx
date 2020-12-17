156b2fa8e4b560333514be5b7077e67634a826fb
ricoagustinus123 <rico_a.m@outlook.com>
2020-12-17 11:23:19 +0700

    update 171220

--- src/App.jsx
+++ src/App.jsx
@@ -1,75 +1,75 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import Card from "./components/card/Card";
 
 function App() {
-  const [comment, setComment] = useState([
-    {
-      id: 1,
-      username: "fadliselaz",
-      body: "test",
-    },
-    {
-      id: 2,
-      username: "evalia",
-      body: "test",
-    },
-    {
-      id: 3,
-      username: "selva",
-      body: "test",
-    },
-    {
-      id: 4,
-      username: "alif",
-      body: "test",
-    },
-  ]);
+  const [show,setShow] = useState(true)
+  const [comment, setComment] = useState();
 
-  const [inputdata, setInputData] = useState({
-    username: "",
-    body: "",
-  });
+  useEffect(()=>{
+    fetch("http://localhost:5000/user-comment",{
+      method:"GET",
+      mode:"cors",
+      headers:{
+        "Content-Type":"application/json",
+      }
+    })
+    .then((res)=>res.json())
+    .then((data)=>{
+      setComment(data)
+    })
+  },[])
+
+ 
 
   return (
     <div className="App">
       <h1>Hello Comment App</h1>
 
-      <div className="form_group">
+      <form action="http://localhost:5000/user-comment" method="post">
+      <div className="form_group" 
+      style={{
+        animationName:show ? "slide_right" :"slide_left"
+      }}
+      >
+      <span className="add_button"
+      onClick={()=>{
+        setShow(!show)
+      }}
+      >Add</span>
+
         <input
           type="text"
           placeholder="masukan username"
-          onChange={(e) => {
-            setInputData({
-              ...inputdata,
-              username: e.target.value,
-            });
-          }}
+          name="username"
         />
 
         <textarea
           type="text"
           placeholder="masukan pesan"
-          onChange={(e) => {
-            setInputData({
-              ...inputdata,
-              body: e.target.value,
-            });
-          }}
+          name="body"
         />
 
-        <button
-          onClick={() => {
-            setComment([...comment, inputdata]);
-          }}
-        >
+        <button>
           submit
         </button>
       </div>
 
-      {comment.map((e) => {
+      <>
+      {comment ? 
+      (<div className="container-card">
+        {comment.map((e) => {
         return <Card username={e.username} body={e.body} />;
-      })}
+        })}
+      </div>)
+      :
+      
+      (
+        <h1>gagal</h1>
+      )}
+      
+      </>
+      </form>
     </div>
   );
 }
--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -2,10 +2,12 @@ import "./Card.css";
 
 const Card = ({ username, body }) => {
   return (
+    <section className="card-container">
     <div className="card">
       <h3 className="comment_username"> {username} </h3>
       <p className="comment_body">{body}</p>
     </div>
+    </section>
   );
 };
 
